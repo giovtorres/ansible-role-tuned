772bc48eb199a85c776263976944578fe646199d
Giovanni Torres <giovanni.torres@gmail.com>
2019-06-30 09:30:08 -0400

    Add scenario for custom profile.
    - Test default and with_custom_profile separately
    - Use newer version of testinfra for Ansible 2.8 compatibility
    - Update molecule test suite to the latest
    - Change when conditionals to avoid bare variable deprecation warning
    - Remove epel dependency and explicit ansible install from Travis CI build config

--- molecule/with_custom_profile/tests/test_default.py
+++ molecule/with_custom_profile/tests/test_default.py
@@ -17,9 +17,18 @@ def test_tuned_running_and_enabled(host):
     assert s.is_enabled
 
 
+def test_tuned_profile_file(host):
+    f = host.file("/etc/tuned/my_custom_profile/tuned.conf")
+    assert f.exists
+    assert f.is_file
+    assert f.user == "root"
+    assert f.group == "root"
+    assert f.mode == 0o644
+
+
 def test_tuned_active_profile(host):
     cmd = "/usr/sbin/tuned-adm active"
-    assert "throughput-performance" in host.check_output(cmd)
+    assert "my_custom_profile" in host.check_output(cmd)
 
 
 def test_tuned_verify(host):
